f944fddf05b1f8790720f5ee0548806505b9984c
Duy Le <leanhduy1998@yahoo.com>
2017-12-03 05:05:51 -0500

    first commit

--- Aquarium Aquameter Tracker/Controller/DetailViewController.h
+++ Aquarium Aquameter Tracker/Controller/DetailViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  DetailViewController.h
 //  Aquarium Aquameter Tracker
 //
-//  Created by Duy Le on 12/1/17.
+//  Created by Duy Le on 12/2/17.
 //  Copyright © 2017 Duy Le. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface DetailViewController : UIViewController
 
 @end
-
